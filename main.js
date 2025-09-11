a7ca2a00a6c49d0552bd502821d83cf84b6e833e
Italo Feitosa Paiva <111617094+Italofpaiva@users.noreply.github.com>
2025-09-11 14:46:46 -0300

    Add the file mainj.s

---
+++ main.js
@@ -0,0 +1,15 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+         navLinks.forEach(link => link.classList.remove("active"));
+
+         const id = entry.target.getAttribute("id");
+      document.querySelector(`.nav-link[href="#${id}"]`).classList.add("active");
+    }
+  });
+}, { threshold: 0.6 }); // ativa quando 60% da seção estiver visível
+
+sections.forEach(section => observer.observe(section));
