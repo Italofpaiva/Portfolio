dc564a7107826c4872042f02b3a23bd4b544383f
Italo Feitosa Paiva <111617094+Italofpaiva@users.noreply.github.com>
2025-09-17 11:28:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,4 +12,10 @@ const observer = new IntersectionObserver((entries) => {
   });
 }, { threshold: 0.6 }); // ativa quando 60% da seção estiver visível
 
+
 sections.forEach(section => observer.observe(section));
+
+window.addEventListener('mousemove', e => {
+  document.documentElement.style.setProperty('--mouse-x', `${e.clientX}px`);
+  document.documentElement.style.setProperty('--mouse-y', `${e.clientY}px`);
+});
